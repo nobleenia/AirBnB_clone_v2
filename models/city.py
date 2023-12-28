6cc2b3d645e08e81fbfa76a0f1c350671b22dd3f
Noble <neluwah@gmail.com>
2023-12-28 09:05:37 -0800

    Update: Corrections

--- models/city.py
+++ models/city.py
@@ -12,4 +12,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    places = relationship("Places", cascade='all, delete, delete-orphan', backref="cities")
+    places = relationship("Place", cascade='all, delete, delete-orphan', backref="cities")
