95b861485d24bb1be4e246ca9a7240a4f8d86d64
Noble <nobleeluwah@gmail.com>
2024-01-28 00:32:13 +0000

    Correction: 0-hello_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,23 +2,22 @@
 """
 A script that starts a Flask web application
 """
-
 from flask import Flask
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_route():
     """
-    display: 'Hello HBNB!'
+    display 'Hello HBNB!'
     """
     return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """
-    display: HBNB
+    display HBNB
     """
     return "HBNB"
 
