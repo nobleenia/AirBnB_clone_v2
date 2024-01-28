eb706a427db55ceb0e35f251d39b0273767015e9
Noble <nobleeluwah@gmail.com>
2024-01-28 00:41:52 +0000

    Update: 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,13 +4,13 @@ A script that starts a Flask web application
 """
 
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """
-    display: 'Hello HBNB!'
+    display 'Hello HBNB!'
     """
     return "Hello HBNB!"
 
